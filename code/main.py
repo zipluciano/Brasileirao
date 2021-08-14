3d1981f0bd1b85b2d982dc9ccef423b723edb694
luciano_es.mo <luciano.es.mo@gmail.com>
2021-08-14 10:40:58 -0300

    Fixing charts

--- code/main.py
+++ code/main.py
@@ -43,7 +43,7 @@ stats['Posse de bola'] = to_num_posse
 stats['Precisão de passe'] = to_num_passe
 # até aqui o conjunto de dados foi preparado para análises futuras.
 # a partir desse ponto será construido um modelo apenas para prever
-# com base na quantidade de gols, se é mandante ou visitante
+# com base na quantidade de chutes a gols, se é mandante ou visitante
 
 # %%
 
@@ -104,24 +104,34 @@ stats.head()
 
 # %%
 
-fig, axes = plt.subplots(1, 2, figsize=(10, 6))
+fig, axes = plt.subplots(1, 2, figsize=(10, 6), sharey = True)
 fig.suptitle('Chutes a Gol - Real', fontsize = 15)
 sns.boxplot(ax = axes[0], data = stats[stats['Mandante'] == 0], y = 'Chutes a gol')
 sns.boxplot(ax = axes[1], data = stats[stats['Mandante'] == 1], y = 'Chutes a gol')
-axes[0].set_xlabel('Mandante', fontsize = 12)
+axes[0].set_xlabel('Visitante', fontsize = 12)
 axes[0].set_ylabel('Chutes a Gol', fontsize = 12)
-axes[1].set_xlabel('Visitante', fontsize = 12)
-axes[1].set_ylabel('Chutes a Gol', fontsize = 12)
+axes[1].set_xlabel('Mandante', fontsize = 12)
+axes[1].set_ylabel('', fontsize = 12)
 plt.show()
 
 # %%
 
-fig, axes = plt.subplots(1, 2, figsize=(10, 6))
+fig, axes = plt.subplots(1, 2, figsize=(10, 6), sharey = True)
 fig.suptitle('Chutes a Gol - Previsto', fontsize = 15)
 sns.boxplot(ax = axes[0], data = comp[comp['alvo_previsto'] == 0], y = 'cht_gol_teste')
 sns.boxplot(ax = axes[1], data = comp[comp['alvo_previsto'] == 1], y = 'cht_gol_teste')
-axes[0].set_xlabel('Mandante', fontsize = 12)
+axes[0].set_xlabel('Visitante', fontsize = 12)
 axes[0].set_ylabel('Chutes a Gol', fontsize = 12)
-axes[1].set_xlabel('Visitante', fontsize = 12)
-axes[1].set_ylabel('Chutes a Gol', fontsize = 12)
+axes[1].set_xlabel('Mandante', fontsize = 12)
+axes[1].set_ylabel('', fontsize = 12)
 plt.show()
+
+# %%
+
+print(model.predict([[2]]))
+
+# %%
+
+print(model.predict([[7]]))
+
+# %%
